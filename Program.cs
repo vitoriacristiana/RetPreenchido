f38b1fbf4d628124f40cf0c8ddf8920ada7a07ea
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-11-25 22:10:44 -0300

    Resolução do execício

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RetPreenchido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tamanho do retângulo:");
+
+            Console.Write ("Largura:");
+            int  largura  = Int32.Parse (Console.ReadLine());
+
+            Console.Write ("Altura .:");
+            int  altura  =  Int32 . Parse ( Console . ReadLine ());
+
+            Console.WriteLine ();
+
+            for (int  x  =  0 ; x  <  altura; x ++)
+            {
+             for (int  y  = 0;y  <  largura; y ++)
+                {
+                Console.Write ( " * " );
+                }
+               Console.WriteLine ();
+             }
+        }
+    }
+}
